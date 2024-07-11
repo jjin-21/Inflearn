78754ee65721c53dedabc7b869989a4673603c73
jjin-21 <ljj1900@naver.com>
2024-07-12 01:27:31 +0900

    AppConfig 수정

--- src/main/java/org/example/inflearn/config/AppConfig.java
+++ src/main/java/org/example/inflearn/config/AppConfig.java
@@ -1,6 +1,9 @@
 package org.example.inflearn.config;
 
+import org.example.inflearn.discount.DiscountPolicy;
 import org.example.inflearn.discount.FixDiscountPolicy;
+import org.example.inflearn.discount.RateDiscountPolicy;
+import org.example.inflearn.member.MemberRepository;
 import org.example.inflearn.member.MemberService;
 import org.example.inflearn.member.MemberServiceImpl;
 import org.example.inflearn.member.MemoryMemberRepository;
@@ -10,10 +13,19 @@ import org.example.inflearn.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memoryMemberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memoryMemberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memoryMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
